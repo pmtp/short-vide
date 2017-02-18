3348894a0d505c63ee695809e8f55e0e2f2e21d7
Pedro Pereira <pedmigtelpereira@gmail.com>
2017-02-18 00:35:39 +0000

    Add seeds.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+         $this->call(TopicsTableSeeder::class);
+         $this->call(VideoReactionsTableSeeder::class);
+         $this->call(VideoReactionVideoSizesTableSeeder::class);
     }
 }
